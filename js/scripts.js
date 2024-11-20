95e60e2b4eca5c716721a6775ee5c671c49878f0
mayoz99 <120589429+mayoz99@users.noreply.github.com>
2024-11-20 18:47:58 -0500

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// This is where you should write all JavaScript
+// for your project. Remember a few things as you start!
+// - Use let or const for all variables
+// - Do not use jQuery - use JavaScript instead
+// - Do not use onclick - use addEventListener instead
+// - Run npm run test regularly to check autograding
+function startGame() {
+    // Navigate to subpage.html
+    window.location.href = 'Subpage.HTML';
+}
